f48a05faf7f09b47607878aa9b4481a1a71e9c6f
wisestMe <wyhzest@gmail.com>
2020-06-22 14:48:27 +0100

    implement hide/show footer menu

--- js/people.js
+++ js/people.js
@@ -22,7 +22,7 @@ $('.slides')
 });
 
 
-// expand menu items
+// expand footer menu items
 
 // Get the plus icons
 var expandButtons = document.querySelectorAll('.open-close');
@@ -39,50 +39,77 @@ var closeButtons = document.querySelectorAll('.fa-window-close');
  var footerMenuList = document.getElementById('footer').getElementsByTagName('li');
  console.log(footerMenuList);
 
- for (footerMenuItem of footerMenuList) {
+// hide footer menu list
+
+function hideFooterMenuList() {
+	for (footerMenuItem of footerMenuList) {
  	footerMenuItem.classList.add('hide');
+ 	footerMenuItem.classList.remove('show');
  }
+}
 
- 
+hideFooterMenuList();
 
-// Loop through all the icons and add event listeners
+// Show footer menu list
+
+function showFooterMenuList() {
+	for (footerMenuItem of footerMenuList) {
+ 	footerMenuItem.classList.add('show');
+ 	footerMenuItem.classList.remove('hide');
+ }
+}
+
+// Hide plus icon
+
+function hidePlusIcons() {
+	for (plusIcon of plusIcons){
+			plusIcon.classList.add('hide');
+			plusIcon.classList.remove('show');
+		}
+}
+
+// Show plus icon
+
+function showPlusIcons() {
+	for (plusIcon of plusIcons){
+			plusIcon.classList.add('show');
+			plusIcon.classList.remove('hide');
+		}
+}
+// Hide close icons
+function hideCloseButton() {
+	for (closeButton of closeButtons) {
+	closeButton.classList.add('hide');
+	closeButton.classList.remove('show');
+}
+}
+
+hideCloseButton()
+
+// Loop through all the plus icons and add event listeners
 for (plusIcon of plusIcons) {
 	plusIcon.addEventListener('click', function() {
 		// hide all the expand buttons
-		for (plusIcon of plusIcons){
-			plusIcon.classList.add('hide');
-		}
+		hidePlusIcons();
 
 		
-		//get the hidden menu items
+		//get the hidden menu items of the clicked portion
 		var menuList = this.parentNode.parentNode.parentNode.childNodes[3].children;
 		
 		// Get current close icon
 		var currentClose = this.parentNode.children[1];
-		console.log(currentClose);
-		currentClose.style.display = 'block';
+		
+		currentClose.classList.add('show');
+		currentClose.classList.remove('hide');
+
 		// Loop through the hidden items and display them
 		for (menuItem of menuList) {
-
-			// menuItem.style.display = 'block';
-			menuItem.classList.toggle('hide');
-			menuItem.classList.toggle('show');
-			//menuItem.style.zIndex = '2';
-
+			menuItem.classList.remove('hide');
+			menuItem.classList.add('show');
 		}	
-		
-		//this.parentNode.parentNode.childNodes[3].childNodes.style.display= 'block';
 	})
 }
 
-function hideCloseButton() {
-	for (closeButton of closeButtons) {
-	closeButton.classList.add('hide');
-	// closeButton.style.zIndex = '-2';
-}
-}
-
-// hideCloseButton();
 
 // add event listener to close icons
 
@@ -91,18 +118,11 @@ for (closeButton of closeButtons) {
 		for (plusIcon of plusIcons){
 			plusIcon.classList.remove('hide');
 			hideCloseButton();
+			showPlusIcons();
 
 			//get the hidden menu items
 		var menuList = this.parentNode.parentNode.parentNode.childNodes[3].children;
-			for (menuItem of menuList) {
-
-			// menuItem.style.display = 'block';
-			
-			menuItem.classList.remove('show');
-			menuItem.classList.add('hide');
-			//menuItem.style.zIndex = '2';
-
 		}
-		}
+		hideFooterMenuList();
 	})
 }
