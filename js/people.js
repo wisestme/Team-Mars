5917a1d4617b5f551b36b26be0bec1c0cd29b917
wisestMe <wyhzest@gmail.com>
2020-06-22 10:38:21 +0100

    adjust position

--- js/people.js
+++ js/people.js
@@ -25,7 +25,12 @@ $('.slides')
 // expand menu items
 
 // Get the plus icons
-var expandButtons = document.querySelectorAll('.fa-plus');
+var expandButtons = document.querySelectorAll('.open-close');
+var plusIcons = document.querySelectorAll('.fa-plus');
+
+// Get the close icons
+var closeButtons = document.querySelectorAll('.fa-window-close');
+
 
 // Get the footer menu
  var footerMenu = document.querySelectorAll('.footer-menu');
@@ -41,19 +46,63 @@ var expandButtons = document.querySelectorAll('.fa-plus');
  
 
 // Loop through all the icons and add event listeners
-for (expandButton of expandButtons) {
-	expandButton.addEventListener('click', function() {
-		//get the hidden menu items
-		var menuList = this.parentNode.parentNode.childNodes[3].children;
+for (plusIcon of plusIcons) {
+	plusIcon.addEventListener('click', function() {
+		// hide all the expand buttons
+		for (plusIcon of plusIcons){
+			plusIcon.classList.add('hide');
+		}
+
 		
+		//get the hidden menu items
+		var menuList = this.parentNode.parentNode.parentNode.childNodes[3].children;
+		
+		// Get current close icon
+		var currentClose = this.parentNode.children[1];
+		console.log(currentClose);
+		currentClose.style.display = 'block';
 		// Loop through the hidden items and display them
 		for (menuItem of menuList) {
+
 			// menuItem.style.display = 'block';
 			menuItem.classList.toggle('hide');
 			menuItem.classList.toggle('show');
 			//menuItem.style.zIndex = '2';
+
 		}	
 		
 		//this.parentNode.parentNode.childNodes[3].childNodes.style.display= 'block';
+	})
+}
+
+function hideCloseButton() {
+	for (closeButton of closeButtons) {
+	closeButton.classList.add('hide');
+	// closeButton.style.zIndex = '-2';
+}
+}
+
+// hideCloseButton();
+
+// add event listener to close icons
+
+for (closeButton of closeButtons) {
+	closeButton.addEventListener('click', function() {
+		for (plusIcon of plusIcons){
+			plusIcon.classList.remove('hide');
+			hideCloseButton();
+
+			//get the hidden menu items
+		var menuList = this.parentNode.parentNode.parentNode.childNodes[3].children;
+			for (menuItem of menuList) {
+
+			// menuItem.style.display = 'block';
+			
+			menuItem.classList.remove('show');
+			menuItem.classList.add('hide');
+			//menuItem.style.zIndex = '2';
+
+		}
+		}
 	})
 }
